daf99a904092d91066c8e39895787e2a2778a408
shoedogodo <46584549+shoedogodo@users.noreply.github.com>
2024-05-14 19:46:05 +0800

    restart

--- app/src/main/java/com/example/notetakingapp/MainActivity.java
+++ app/src/main/java/com/example/notetakingapp/MainActivity.java
@@ -1,14 +1,80 @@
 package com.example.notetakingapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+//import com.google.android.gms.tasks.OnCompleteListener;
+//import com.google.android.gms.tasks.Task;
+//import com.google.firebase.auth.AuthResult;
+//import com.google.firebase.auth.FirebaseAuth;
+//import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText mloginemail,mloginpassword;
+    private RelativeLayout mlogin,mgotosignup;
+    private  TextView mgotoforgotpassword;
+
+    //private FirebaseAuth firebaseAuth;
+
+    ProgressBar mprogressbarofmainactivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+
+        mloginemail=findViewById(R.id.loginemail);
+        mloginpassword=findViewById(R.id.loginpassword);
+        mlogin=findViewById(R.id.login);
+        mgotoforgotpassword=findViewById(R.id.gotoforgotpassword);
+        mgotosignup=findViewById(R.id.gotosignup);
+        mprogressbarofmainactivity=findViewById(R.id.progressbarofmainactivity);
+
+        //firebaseAuth=FirebaseAuth.getInstance();
+        //FirebaseUser firebaseUser=firebaseAuth.getCurrentUser();
+
+        mgotosignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,signup.class));
+            }
+        });
+
+        mgotoforgotpassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,forgotpassword.class));
+            }
+        });
+
+        mlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String mail=mloginemail.getText().toString().trim();
+                String password=mloginpassword.getText().toString().trim();
+
+                if(mail.isEmpty()|| password.isEmpty())
+                {
+                    Toast.makeText(getApplicationContext(),"All Field Are Required",Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    // login the user
+                }
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/notetakingapp/signup.java
+++ app/src/main/java/com/example/notetakingapp/signup.java
@@ -1,14 +1,73 @@
 package com.example.notetakingapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+//import com.google.android.gms.tasks.OnCompleteListener;
+//import com.google.android.gms.tasks.Task;
+//import com.google.firebase.auth.AuthResult;
+//import com.google.firebase.auth.FirebaseAuth;
+//import com.google.firebase.auth.FirebaseUser;
 
 public class signup extends AppCompatActivity {
+
+    private EditText msignupemail,msignuppassword;
+    private RelativeLayout msignup;
+    private TextView mgotologin;
+
+
+    //private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        getSupportActionBar().hide();
+
+        msignupemail=findViewById(R.id.signupemail);
+        msignuppassword=findViewById(R.id.signuppassword);
+        msignup=findViewById(R.id.signup);
+        mgotologin=findViewById(R.id.gotologin);
+
+        //firebaseAuth= FirebaseAuth.getInstance();
+
+        mgotologin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(signup.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        msignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String mail = msignupemail.getText().toString().trim();
+                String password = msignuppassword.getText().toString().trim();
+
+                if (mail.isEmpty() || password.isEmpty())
+                {
+                    Toast.makeText(getApplicationContext(), "All Fields are Required", Toast.LENGTH_SHORT).show();
+                }
+                else if (password.length() < 7)
+                {
+                    Toast.makeText(getApplicationContext(), "Password Should Greater than 7 Digits", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    /// registered the user to firebase
+                }
+            }
+        });
     }
 }
